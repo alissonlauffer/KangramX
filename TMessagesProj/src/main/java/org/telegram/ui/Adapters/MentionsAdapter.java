34193de1655403de4f448e5b9e6095bb0d997a31
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-02-25 11:40:39 +0800

    Long press to mention users by name

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/MentionsAdapter.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/MentionsAdapter.java
@@ -1001,7 +1001,7 @@ public class MentionsAdapter extends RecyclerListView.SelectionAdapter {
     }
 
     public boolean isLongClickEnabled() {
-        return searchResultHashtags != null || searchResultCommands != null;
+        return searchResultHashtags != null || searchResultCommands != null || searchResultUsernames != null;
     }
 
     public boolean isBotCommands() {
