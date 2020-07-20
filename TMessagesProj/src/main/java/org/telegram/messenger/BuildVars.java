11f7ec5e3a3d3feee0d31b6e11734a096ea83f8b
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-07-20 01:49:53 -0300

    Fixed build.config

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -21,8 +21,8 @@ public class BuildVars {
     public static boolean TON_WALLET_STANDALONE = false;
     public static int BUILD_VERSION = 2042;
     public static String BUILD_VERSION_STRING = "6.3.0";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = BuildConfig.APP_ID;
+    public static String APP_HASH = BuildConfig.APP_HASH;
     //
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
