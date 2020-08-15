9fe7f2572a0064071f0fd7aa511219434a7c6608
Alisson Lauffer <alissonvitortc@gmail.com>
2020-08-15 19:53:26 -0300

    Revert "[TF][JNI][UPDATE] BoringSSL from source (master)"
    This reverts commit 11e759431ed405d8a64d77470c5e3d27be8ab485.

--- TMessagesProj/jni/Android.mk
+++ TMessagesProj/jni/Android.mk
@@ -104,13 +104,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := crypto
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    LOCAL_SRC_FILES := ./boringssl/build/armeabi-v7a/crypto/libcrypto.a
+    LOCAL_SRC_FILES := ./boringssl/lib/libcrypto_armeabi-v7a.a
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-    LOCAL_SRC_FILES := ./boringssl/build/arm64-v8a/crypto/libcrypto.a
+    LOCAL_SRC_FILES := ./boringssl/lib/libcrypto_arm64-v8a.a
 else ifeq ($(TARGET_ARCH_ABI),x86)
-    LOCAL_SRC_FILES := ./boringssl/build/x86/crypto/libcrypto.a
+    LOCAL_SRC_FILES := ./boringssl/lib/libcrypto_x86.a
 else ifeq ($(TARGET_ARCH_ABI),x86_64)
-    LOCAL_SRC_FILES := ./boringssl/build/x86_64/crypto/libcrypto.a
+    LOCAL_SRC_FILES := ./boringssl/lib/libcrypto_x86_64.a
 endif
 
 include $(PREBUILT_STATIC_LIBRARY)
